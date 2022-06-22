914dd67d71f8fadd32ab7d84914a9da5720744f8
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 03:43:43 +0900

    feat: 사용자 모델 작성

--- src/api/users/models.py
+++ src/api/users/models.py
@@ -1,3 +1,54 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+
+    MBTI_CHOICES = (
+        ("XXXX", "XXXX"),
+        ("ISTJ", "ISTJ"),
+        ("ISTP", "ISTP"),
+        ("ISFJ", "ISFJ"),
+        ("INTJ", "INTJ"),
+        ("ESTJ", "ESTJ"),
+        ("ISFP", "ISFP"),
+        ("INFJ", "INFJ"),
+        ("ESFJ", "ESFJ"),
+        ("ESFP", "ESFP"),
+        ("INFP", "INFP"),
+        ("INTP", "INTP"),
+        ("ESTP", "ESTP"),
+        ("ENTJ", "ENTJ"),
+        ("ENTP", "ENTP"),
+        ("ENFP", "ENFP"),
+        ("ENFJ", "ENFJ"),
+    )
+
+    mbti = models.CharField(
+        max_length=4,
+        default="XXXX",
+        choices=MBTI_CHOICES
+    )
+    email = models.EmailField(
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+    username = models.BigIntegerField(
+        unique=True,
+    )
+    password = models.CharField(
+        max_length=50
+    )
+    about = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "user"
+
+    def __str__(self):
+        return f"{self.username}"
