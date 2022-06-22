a9fffd4df2e9d558638339fabe9e51bbf1c77982
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 03:45:38 +0900

    fix: 중복 Path 오타 수정

--- src/api/users/urls.py
+++ src/api/users/urls.py
@@ -4,7 +4,7 @@ from users import views
 
 urlpatterns = [
     # 카카오 로그인
-    path('users/login/', views.kakao_login, name='카카오 로그인'),
-    path('users/login/callback/', views.kakao_callback, name='카카오 로그인 콜백'),
-    path('users/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
+    path('login/', views.kakao_login, name='카카오 로그인'),
+    path('login/callback/', views.kakao_callback, name='카카오 로그인 콜백'),
+    path('login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
 ]
