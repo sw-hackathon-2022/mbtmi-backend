119a5027116f59c4e978fb7d43126955ee2c6f10
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 17:11:03 +0900

    update: 게시글 반응 -> reactions로 구조화, 스키마 추가

--- src/api/posts/serializers.py
+++ src/api/posts/serializers.py
@@ -1,21 +1,17 @@
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema, extend_schema_field, extend_schema_serializer
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField, CurrentUserDefault
-from rest_framework.serializers import ListSerializer
+from rest_framework.fields import SerializerMethodField
+from rest_framework.relations import PrimaryKeyRelatedField
 
 from comments.serializers import CommentSerializer
 from posts.models import Post
-from reactions.serializers import PostLikeSerializer, PostUnlikeSerializer
+from reactions.serializers import PostLikeSerializer, PostUnlikeSerializer, PostReactionCountSerializer
 from users.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
-    comments = CommentSerializer(many=True, source="comment_set",
-                                 context={"login_user": CurrentUserDefault()})
-    like_count = SerializerMethodField()
-    unlike_count = SerializerMethodField()
+    author = UserSerializer(read_only=True)
+    reactions = SerializerMethodField()
 
     class Meta:
         model = Post
@@ -25,34 +21,18 @@ class PostSerializer(serializers.ModelSerializer):
             "title",
             "content",
             "created_at",
-            "comments",
-            "like_count",
-            "unlike_count"
+            "reactions",
         ]
         read_only_fields = (
             "id",
             "author",
             "created_at",
-            "comments",
-            "like_count",
-            "unlike_count"
+            "reactions",
         )
 
-    @extend_schema_field(ListSerializer(child=CommentSerializer()))
-    def get_comments(self, obj: Post):
-        return CommentSerializer(obj.comment_set, many=True,
-                                 source="comment_set",
-                                 context={
-                                     "request": self.context.get("request")}
-                                 ).data
-
-    @extend_schema_field(serializers.IntegerField)
-    def get_like_count(self, obj: Post):
-        return obj.like_set.count()
-
-    @extend_schema_field(serializers.IntegerField)
-    def get_unlike_count(self, obj: Post):
-        return obj.unlike_set.count()
+    @extend_schema_field(PostReactionCountSerializer)
+    def get_reactions(self, obj: Post):
+        return PostReactionCountSerializer(obj).data
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
