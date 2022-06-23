2bd87123e858243abb14b82027d59859a059c3e8
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 17:10:27 +0900

    fix: 목록 조회 시에만 필터 사용하도록 수정

--- src/api/posts/filters.py
+++ src/api/posts/filters.py
@@ -2,12 +2,18 @@ from django.db.models import Q
 from rest_framework import filters
 from rest_framework.request import Request
 
+from posts.models import Post
+
 
 class PostByMBTIFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request: Request, queryset, view):
-        mbti_candidates = request.query_params.get("mbti").split(",")  #
-        _filter = Q()
-        for candidate in mbti_candidates:
-            _filter |= Q(author__mbti=candidate)
-        queryset.filter(_filter)
-        return queryset.filter(queryset)
+        if request.method == "GET":
+            mbti_query_string = request.query_params.get("mbti", None)
+            if mbti_query_string is None:
+                return Post.objects.none()
+            mbti_candidates = mbti_query_string.split(",")
+            _filter = Q()
+            for candidate in mbti_candidates:
+                _filter |= Q(author__mbti__icontains=candidate)
+            return queryset.filter(_filter)
+        return queryset
