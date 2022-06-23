54911cb0a57f7e14ad44e33e9dc5f9d2392df5ae
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 15:59:53 +0900

    chore: 스키마 수정, 상세조회 추가중, 필터링 구현중

--- src/api/posts/views.py
+++ src/api/posts/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter
 from rest_framework import viewsets
+from rest_framework.generics import RetrieveAPIView
 
+from posts.filters import PostByMBTIFilterBackend
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, PostDetailSerializer
 
 
 @extend_schema_view(
-    operation_id="게시글",
-    tags=["/posts"],
     list=extend_schema(
+        tags=["/posts"],
         operation_id="게시글 목록 조회",
         description="게시글 정보/반응 목록 조회",
+        parameters=[
+            OpenApiParameter(name="mbti", description="(필터) 작성자 MBTI(ex. mbti=E,S,I,F,J,T", type=str)
+        ]
     ),
     create=extend_schema(
+        tags=["/posts"],
         operation_id="게시글 등록",
         description="제목/본문 정보로 게시글 등록",
     ),
-    put=extend_schema(
+    update=extend_schema(
+        tags=["/posts"],
         operation_id="게시글 수정",
         description="게시글 제목/본문 수정",
+    ),
+    destroy=extend_schema(
+        tags=["/posts"],
+        operation_id="게시글 삭제",
+        description="게시글 삭제",
     ),
 )
 class PostModelViewSet(viewsets.ModelViewSet):
@@ -31,4 +43,18 @@ class PostModelViewSet(viewsets.ModelViewSet):
         "put",
         "delete"
     ]
+    lookup_field = "id"
+    lookup_url_kwarg = "post_id"
+    filter_backends = (PostByMBTIFilterBackend,)
 
+
+@extend_schema(
+    tags=["/posts"],
+    operation_id="게시글 상세 조회",
+    description="게시글 정보/반응수, 반응여부 조회",
+)
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.select_related("author").all()
+    serializer_class = PostDetailSerializer
+    http_method_names = ["get"]
+    lookup_field = "id"
