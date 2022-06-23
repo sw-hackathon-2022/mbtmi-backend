a04e22d424491880eea8a2e7cf062c785f0f7992
Dahee Goo <heeouo@kookmin.ac.kr>
2022-06-23 16:00:04 +0900

    chore: 목록조회/수정/삭제 + 상세조회

--- src/api/posts/urls.py
+++ src/api/posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from posts.views import PostModelViewSet
+from posts.views import PostModelViewSet, PostDetailView
 
 router = routers.SimpleRouter()
 router.register(r'', PostModelViewSet)
 
 urlpatterns = [
-    path("", include(router.urls), name="게시글")
+    path("", include(router.urls), name="게시글"),
+    path("<int:post_id>/", PostDetailView.as_view({"get": "retrieve"}), name="게시글 상세 조회")
 ]
